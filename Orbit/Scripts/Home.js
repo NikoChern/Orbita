bd470e7d7f95440d93b3060db2e4e26465220871
NikoChern <ncernavskij@gmail.com>
2016-06-18 16:23:38 +0300

    The latest version Orbita

---
+++ Orbit/Scripts/Home.js
@@ -0,0 +1,15 @@
+﻿function menuClick(e, a) {
+    document.getElementById("menu-popup").style.display = "block";
+}
+
+document.onclick = function (e, a) {
+    var el = e.target;
+    while (el) {
+        if (el.id == "menu-popup" || el.id == "menu" && (e.target.nodeName == "A" || e.target.parentNode.nodeName == "A"))
+            return;
+
+        el = el.parentNode;
+    }
+
+    document.getElementById("menu-popup").style.display = "none";
+};
